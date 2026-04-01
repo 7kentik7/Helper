32ac59694dee7376b0c5d957b9474af4d3c3f2b5
kroll <maximantyufeev13@yandex.ru>
2026-04-01 16:37:53 +0400

    HelperItem

--- app/src/main/java/com/example/helperjc/presentationJC/HelperScreen.kt
+++ app/src/main/java/com/example/helperjc/presentationJC/HelperScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -20,6 +21,7 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -32,43 +34,72 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.helperjc.R
+import com.example.helperjc.domain.plandetails.PlanDetails
+import com.example.helperjc.domain.plans.Plan
+import com.example.helperjc.parseToString
 import com.example.helperjc.presentationJC.ui.theme.HelperJCTheme
+import com.example.helperjc.presentationJC.ui.theme.ProgressFifthStep
+import com.example.helperjc.presentationJC.ui.theme.ProgressFirstStep
+import com.example.helperjc.presentationJC.ui.theme.ProgressFourthStep
+import com.example.helperjc.presentationJC.ui.theme.ProgressSecondStep
+import com.example.helperjc.presentationJC.ui.theme.ProgressThirdStep
 
 
 @Composable
 fun HelperScreen() {
-
     Scaffold(
         topBar = { AppBar() },
-        containerColor = MaterialTheme.colorScheme.background
-
+        containerColor = MaterialTheme.colorScheme.background,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            ) {
+                Icon(Icons.Filled.Add, "Floating action button.")
+            }
+        }
     ) { innerPadding ->
         PlanItem(
-            title = "План",
             innerPadding = innerPadding,
-            countOfTask = 1,
-            countOfCompletedTask = 2,
-            progress = 0.3f,
-            deadLine = "20.02.2026"
+            planDetails = PlanDetails(
+                plan = Plan(title = "План", endTime = null),
+                progress = 0,
+                countOfTasks = 3,
+                countOfCompletedTasks = 2,
+                tasks = listOf()
+            )
         )
+
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun AppBar(
-
-) {
+private fun AppBar() {
     TopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background),
         actions = {
             IconButton(onClick = {}) {
-                Icon(Icons.Default.AccountCircle, contentDescription = null)
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = ImageVector.vectorResource(R.drawable.notes_icon),
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = ImageVector.vectorResource(R.drawable.tasks_icon),
+                    contentDescription = null
+                )
             }
         },
         title = {},
@@ -80,16 +111,9 @@ private fun AppBar(
     )
 }
 
-@Composable
-private fun PlanItem(
-    innerPadding: PaddingValues,
-    deadLine: String,
-    title: String,
-    progress: Float,
-    countOfTask: Int,
-    countOfCompletedTask: Int,
 
-    ) {
+@Composable
+private fun PlanItem(innerPadding: PaddingValues, planDetails: PlanDetails) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -102,9 +126,11 @@ private fun PlanItem(
         onClick = {}
     ) {
         Column(modifier = Modifier.padding(8.dp)) {
-            Text(text = deadLine, fontSize = 14.sp)
-            Text(text = title, fontSize = 18.sp)
-            ProgressBarForItemHelper(progress)
+            planDetails.plan.endTime?.parseToString()?.let {
+                Text(text = it, fontSize = 14.sp, color = MaterialTheme.colorScheme.secondary)
+            }
+            Text(text = planDetails.plan.title, fontSize = 18.sp)
+            ProgressBarForItemHelper(planDetails.progress)
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
@@ -113,21 +139,23 @@ private fun PlanItem(
             ) {
                 Column() {
                     Text(
-                        text = stringResource(id = R.string.tasks_count, countOfTask),
+                        text = stringResource(id = R.string.tasks_count, planDetails.countOfTasks),
                         fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.secondary
                     )
                     Text(
                         text = stringResource(
                             id = R.string.completed_tasks_count,
-                            countOfCompletedTask
+                            planDetails.countOfCompletedTasks
                         ),
-                        fontSize = 14.sp
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.secondary
                     )
                 }
 
                 IconButton(onClick = {}) {
                     Icon(
-                        Icons.Default.Add,
+                        imageVector = ImageVector.vectorResource(id = R.drawable.add_task),
                         contentDescription = null,
                         tint = MaterialTheme.colorScheme.primary,
                         modifier = Modifier.size(24.dp)
@@ -139,24 +167,52 @@ private fun PlanItem(
 }
 
 @Composable
-fun ProgressBarForItemHelper(progress: Float) {
+fun ProgressBarForItemHelper(progress: Int) {
+    val progressFloat = progress / 100f
+
+    val color = when (progress) {
+        in 0 until 20 -> {
+            ProgressFirstStep
+        }
+
+        in 20 until 40 -> {
+            ProgressSecondStep
+        }
+
+        in 40 until 60 -> {
+            ProgressThirdStep
+        }
+
+        in 60 until 80 -> {
+            ProgressFourthStep
+        }
+
+        in 80..100 -> {
+            ProgressFifthStep
+        }
+
+
+        else -> {
+            ProgressFirstStep
+        }
+    }
     Box(
         modifier = Modifier
             .fillMaxWidth()
-
             .height(4.dp)
             .clip(RoundedCornerShape(50))
             .background(MaterialTheme.colorScheme.surfaceVariant)
     ) {
         Box(
             modifier = Modifier
-                .fillMaxWidth(progress)
+                .fillMaxWidth(progressFloat)
                 .fillMaxHeight()
                 .clip(RoundedCornerShape(50))
-                .background(Color.Cyan)
+                .background(color)
         )
     }
 }
+
 
 @Preview
 @Composable
